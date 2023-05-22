953743b77cf9de16a46770f6721c922ee63c544a
Olivier Contant <ocontant@cloudoric.com>
2023-05-21 21:47:15 -0400

    Integrate multi-stream logging output and yaml configuration

--- api/utils/LoggingSystem.js
+++ api/utils/LoggingSystem.js
@@ -1,36 +1,66 @@
-const pino = require('pino');
+const fs = require('fs');
+const yaml = require('js-yaml');
+const pino = process.env.NODE_ENV === 'development' ? require('pino-caller')(require('pino')(), { relativeTo: __dirname }) : require('pino')()
+const pinoms = require('pino-multi-stream').multistream
+const pinoFluent = require('pino-fluentd')
+const pinoSocket = require('pino-socket')
+const pinoLambda = require('pino-lambda')
+const pinomongodb = require('pino-mongodb')
+const pinoPretty = require('pino-pretty')
+
+const levels = {
+  TRACE:  10,
+  DEBUG:  20,
+  INFO:   30,
+  WARN:   40,
+  ERROR:  50,
+  FATAL:  60
+};
+
+// Read configuration file
+let config;
+try {
+  config = yaml.safeLoad(fs.readFileSync('../conf/logging-conf.yaml', 'utf8'));
+} catch (e) {
+  throw new Error(`Failed to load configuration from config.yaml. Please make sure the file exists and is correctly formatted. Original error: ${e.message}`);
+}
+
+// Convert the redactPatterns to a map of regular expressions objects
+const redactPatterns = config.redactPatterns.map(pattern => new RegExp(pattern, 'i'));
+
+// Create an array to hold your log streams
+let streams = []
+
+// For each output destination, if it is enabled in the config,
+// create a new stream and push it into the streams array
+if (config.output.console) {
+  const prettyStream = pinoPretty({ colorize: true, translateTime: 'yyyy-mm-dd HH:MM:ss' });
+  streams.push({ stream: prettyStream, level: process.env.LEVEL })
+}
+if (config.output.fluentd.enabled) {
+  let fluentTransport = pinoFluent({ tag: config.output.fluentd.tag, server: config.output.fluentd.server })
+  streams.push({ stream: fluentTransport, level: process.env.LEVEL })
+}
+if (config.output.syslog.enabled) {
+  let syslogTransport = pinoSocket({ address: config.output.syslog.address, port: config.output.syslog.port })
+  streams.push({ stream: syslogTransport, level: process.env.LEVEL })
+}
+if (config.output.lambda.enabled) {
+  let lambdaTransport = pinoLambda()
+  streams.push({ stream: lambdaTransport, level: process.env.LEVEL })
+}
+if (config.output.mongodb.enabled) {
+  let mongodbTransport = pinomongodb({ mongoURL: config.output.mongodb.mongoURL })
+  streams.push({ stream: mongodbTransport, level: process.env.LEVEL })
+}
+
 
 const logger = pino({
-  level: 'info',
-  redact: {
-    paths: [  // List of Paths to redact from the logs (https://getpino.io/#/docs/redaction)
-      'env.OPENAI_KEY',
-      'env.BINGAI_TOKEN',
-      'env.CHATGPT_TOKEN',
-      'env.MEILI_MASTER_KEY',
-      'env.GOOGLE_CLIENT_SECRET',
-      'env.JWT_SECRET_DEV',
-      'env.JWT_SECRET_PROD',
-      'newUser.password'], // See example to filter object class instances
-    censor: '***', // Redaction character
+  redact: {                       // See example to filter object class instances directly
+    paths: config.redactedPaths,  // List of Paths to redact from the logs (https://getpino.io/#/docs/redaction)
+    censor: '***',                // Redaction characters
   },
-});
-
-// Sanitize outside the logger paths. This is useful for sanitizing variables directly with Regex and patterns.
-const redactPatterns = [ // Array of regular expressions for redacting patterns
-  /api[-_]?key/i, 
-  /password/i,
-  /token/i,
-  /secret/i,
-  /key/i,
-  /certificate/i,
-  /client[-_]?id/i,
-  /authorization[-_]?code/i,
-  /authorization[-_]?login[-_]?hint/i,
-  /authorization[-_]?acr[-_]?values/i,
-  /authorization[-_]?response[-_]?mode/i,
-  /authorization[-_]?nonce/i
-]; 
+}, pinoms(streams));              // Output to all streams in the streams array enabled in ./app/config/logging-conf.yaml
 
 /*
   // Example of redacting sensitive data from object class instances
@@ -43,21 +73,8 @@ const redactPatterns = [ // Array of regular expressions for redacting patterns
     }
     return obj;
   }
-
-  // Example of redacting sensitive data from object class instances
   logger.info({ newUser: redactSensitiveData(newUser) }, 'newUser');
 */
-
-const levels = {
-  TRACE:  10,
-  DEBUG:  20,
-  INFO:   30,
-  WARN:   40,
-  ERROR:  50,
-  FATAL:  60
-};
-
-
 
 let level = levels.INFO;
 
@@ -90,19 +107,16 @@ module.exports = {
       logger.fatal(msg);
     },
 
-    // Custom loggers
-    parameters: (parameters) => {
-      if (level <= levels.TRACE) return;
-      logger.debug({ parameters }, 'Function Parameters');
+    // Middleware functions to log request and response
+    // Logs the request body and query parameters
+    // app.use(log.request()); 
+    request: () => (req, res, next) => {
+      if (level < levels.DEBUG) return next();
+      logger.debug({ query: req.query, body: req.body }, `Hit URL ${req.url} with following`);
+      return next();
     },
-    functionName: (name) => {
-      if (level <= levels.TRACE) return;
-      logger.debug(`EXECUTING: ${name}`);
-    },
-    flow: (flow) => {
-      if (level <= levels.INFO) return;
-      logger.debug(`BEGIN FLOW: ${flow}`);
-    },
+
+    // Redact variables value if they contain sensitive information
     variable: ({ name, value }) => {
       if (level <= levels.DEBUG) return;
       // Check if the variable name matches any of the redact patterns and redact the value
@@ -114,12 +128,22 @@ module.exports = {
         }
       }
       logger.debug({ variable: { name, value: sanitizedValue } }, `VARIABLE ${name}`);
-    },
-    request: () => (req, res, next) => {
-      if (level < levels.DEBUG) return next();
-      logger.debug({ query: req.query, body: req.body }, `Hit URL ${req.url} with following`);
-      return next();
     }
+
+/* TO REMOVE
+    parameters: (parameters) => {
+      if (level <= levels.TRACE) return;
+      logger.debug({ parameters }, 'Function Parameters');
+    },
+    functionName: (name) => {
+      if (level <= levels.TRACE) return;
+      logger.debug(`EXECUTING: ${name}`);
+    },
+    flow: (flow) => {
+      if (level <= levels.INFO) return;
+      logger.debug(`BEGIN FLOW: ${flow}`);
+    }, 
+*/
   }
 };
 
